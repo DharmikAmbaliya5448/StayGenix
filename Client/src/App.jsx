114cca85a0bc4be30a416017d26323a3137f576c
DharmikAmbaliya5448 <dharmikambaliya153@gmail.com>
2025-06-05 15:05:18 +0530

    Completed Admin UI and Different Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,17 +5,30 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import LayOut from "./pages/HotelOwner/LayOut";
+import DashBoard from "./pages/HotelOwner/DashBoard";
+import AddRoom from "./pages/HotelOwner/AddRoom";
+import ListRoom from "./pages/HotelOwner/ListRoom";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<LayOut />}>
+            <Route index element={<DashBoard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
